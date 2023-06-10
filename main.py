3b43ebe1ec5715361dc662efc33afac45ef387fc
Funtime <Funtime.UwU.mail@gmail.com>
2023-06-11 00:56:09 +0330

    b ounce

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import math
 # MASS UNITS   :   KILO GRAMS
 # LENGTH UNITS :   METERS
 # TEMP UNITS :     KELVINS
-# PRESSURE UNITS : PASCALS
 # TIME UNITS :     SECONDS
+
+# ENERGY :         JULES
+# PRESSURE UNITS : PASCALS
+# DENSITY :        KILO GRAMS / METERS
 
 # cordinates are measured by length units
 
@@ -27,17 +30,19 @@ class ball():
         self.turtle_obj.speed(0)
         self.turtle_obj.showturtle()
  
-    def __init__(self, mass: int, radius: int, position: list[int] = [0, 0], temprature:int = RoomTemprature, pressure:int = P0):
+    def __init__(self, mass: int, radius: int, position: list[int] = [0, 0], temprature:int = RoomTemprature, pressure:int = P0, coefficient_of_restitution = 0.5):
         self.mass = mass
         self.radius = radius
-        self.volume = (4/3) * math.pi * (radius**3) 
+        self.volume = (4/3) * math.pi * (radius**3)
         self.density = self.mass / self.volume
         self.temprature = temprature
         self.pressure = pressure
         self.position = position
         self.acceleration = [0 , 0]
+        self.internal_energy = 0;
         self.force = [0 , 0]
         self.speed = [0 , 0]
+        self.coefficient_of_restitution = coefficient_of_restitution
         self.dt = 0
         self.init_turtle()
 
@@ -63,20 +68,16 @@ class ball():
     def applyForce(self, force: list[int]):
         self.force[0] += force[0]
         self.force[1] += force[1]
-        self.acceleration[0] += self.force[0] * self.dt
-        self.acceleration[1] += self.force[1] * self.dt * UnitLength
+        self.acceleration[0] += self.force[0] * UnitLength * self.mass
+        self.acceleration[1] += self.force[1] * UnitLength * self.mass
         self.force[0] -= force[0]
         self.force[1] -= force[1]
 
-B = ball(mass = 0.5, radius = 1, position = [0 , 200])
+    def calculateBounceUp(self, force: list[int]):
+        bounce_force = [force[0], (- self.speed[1]) + (- (self.speed[1] * self.coefficient_of_restitution)) ] 
+        return bounce_force
 
-"""
-TODO:
-    Add turtle.Screen
-    Add another thread for Screen that listens to keyboard
-    In main thread, condition tree for valid key presses
-    - Apply proper force based on what is pressed
-"""
+B = ball(mass = 0.5, radius = 1, position = [0 , 200], coefficient_of_restitution = 0.88)
 
 gravity_force = [0 , -9.8]
 
@@ -84,10 +85,17 @@ old_t = time.time()
 while 1:
     new_t = time.time()
     delta_t = new_t - old_t
+    if (delta_t < 0.00000001):
+        delta_t = 0.00000001
     B.setDt(delta_t)
     old_t = new_t 
 
-    B.applyForce(gravity_force)
+    current_force = [gravity_force[0] * delta_t, gravity_force[1] * delta_t]
+
+    if (B.position[1] < -100):
+        current_force = B.calculateBounceUp(current_force)
+    
+    B.applyForce(current_force)
     B.update()
     B.display()
 
